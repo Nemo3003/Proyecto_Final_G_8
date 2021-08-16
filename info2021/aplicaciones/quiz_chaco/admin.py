f62fdce04cdab2c5b83abc6a6848f9e417e9c981
Ulises <ulimarian2000@gmail.com>
2021-08-16 08:25:38 -0300

    Preguntas y Respuestas, admin arreglado

--- info2021/aplicaciones/quiz_chaco/admin.py
+++ info2021/aplicaciones/quiz_chaco/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
-from .models import Pregunta
-    # Register your models here.
-class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+from .models import Pregunta, Respuesta, Intentos_respuesta
+from .forms import ElegirInlineFormset
 
-    admin.site.register(Pregunta)
+class RespuestaInline(admin.TabularInline):
+    model = Respuesta
+    can_delete = False
+    max_num = Respuesta.maximo_elecciones
+    min_num = Respuesta.minimo_elecciones
+    formset = ElegirInlineFormset
+
+class PreguntaAdmin(admin.ModelAdmin):
+    model = Pregunta
+    inlines = (RespuestaInline, )
+    list_display = ["titulo", ]
+    search_fields = ['titulo', 'pregunta__texto']
+
+
+class Intentos_respuestaAdmin(admin.ModelAdmin):
+    list_display = ["pregunta", "respuesta","correcta","puntaje"]
+    
+    class Meta:
+        model = Intentos_respuesta
+admin.site.register(Pregunta, PreguntaAdmin)
+admin.site.register(Respuesta)
+admin.site.register(Intentos_respuesta)
--- info2021/aplicaciones/quiz_chaco/models.py
+++ info2021/aplicaciones/quiz_chaco/models.py
@@ -1,20 +1,41 @@
 from django.db import models
-# Create your models here.
+from django.conf import settings
+
 from django.contrib.auth.models import User
-
+#from login_register.models import Usuario
 # Create your models here.
-class Pregunta(models.Model):
-    title = models.TextField(null= True, blank= True)
-    status = models.CharField(default='inactive', max_length=50)
-    created_by = models.ForeignKey(User, null= True, blank= True, on_delete = models.CASCADE)
-    pregunta=models.CharField(max_length=600, null=True)
-    opcion1=models.CharField(max_length=200, null=True)
-    opcion2=models.CharField(max_length=200,null=True)
-    opcion3=models.CharField(max_length=200, null=True)
-    opcion4=models.CharField(max_length=200, null=True)
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    modify = models.BooleanField()
-    
+class Pregunta(models.Model):
+
+    numero_respuestas_disponibles = 1
+    titulo = models.TextField(verbose_name="texto de pregunta", null=True)
+
     def __str__(self):
-        return self.title
+            if self.titulo==None:
+               return "No title found"
+            return self.titulo
+
+class Respuesta(models.Model):
+    
+    maximo_elecciones = 4
+    minimo_elecciones = 2
+
+    pregunta = models.ForeignKey(Pregunta, related_name="pregunta", on_delete=models.CASCADE)
+    correcta = models.BooleanField(verbose_name = "¿Es esta la pregunta correcta?", default=False, null=False)
+    titulo = models.TextField(verbose_name = "Texto de la respuesta")
+
+    def __str__(self):
+        return self.titulo
+
+class Usuario(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    puntaje_total = models.DecimalField(verbose_name="Puntaje total", default=0, decimal_places=2, max_digits=10)
+
+class Intentos_respuesta(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+    respuesta = models.ForeignKey(Respuesta, on_delete=models.CASCADE, related_name="intentos")
+    correcta = models.BooleanField(verbose_name = "¿Es la respuesta correcta?", default = False, null = False)
+    puntaje = models.DecimalField(verbose_name="puntaje obtenido", default=0, decimal_places=2, max_digits=6)
+
+
