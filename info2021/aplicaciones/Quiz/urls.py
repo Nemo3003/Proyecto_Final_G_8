a0db657545f8ea6729499fa8649c670dfc7f17d1
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2021-08-23 13:48:24 +0000

    Update urls.py

--- info2021/aplicaciones/Quiz/urls.py
+++ info2021/aplicaciones/Quiz/urls.py
@@ -5,13 +5,13 @@ from .views import (
 			jugar,
 			resultado_pregunta,
 			tablero,)
-
+#Powered by Nemo with help from Google
 urlpatterns = [
 	path('HomeUsuario/', HomeUsuario, name='HomeUsuario'),
 	path('Team/index.html/', team, name='team'),
 
 	path('tablero/', tablero, name='tablero'),  
-	
+#Powered by Nemo with help from Google
 	path('jugar/', jugar, name='jugar'),
 	path('resultado/<int:pregunta_respondida_pk>/', resultado_pregunta, name='resultado'),
 ]
