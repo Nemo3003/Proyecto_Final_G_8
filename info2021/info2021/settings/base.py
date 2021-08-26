2290b7b428bd14e9af3129665aab1d66bff04b2c
Keith <kmld1999@gmail.com>
2021-08-26 19:25:45 -0300

    Correccion del FATAL error

--- info2021/info2021/settings/base.py
+++ info2021/info2021/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from django.apps import AppConfig
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,3 +145,5 @@ LOGOUT_REDIRECT_URL  =  'home'
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
+
+AppConfig.default = False
